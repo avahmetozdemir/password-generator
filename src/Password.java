571c329d3b72db18392f544c7ae9652f284e83bf
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-03 17:36:28 +0300

    04- add passwordstrength func. to see the score of password

--- src/Password.java
+++ src/Password.java
@@ -36,4 +36,38 @@ public class Password {
         return value;
     }
 
+    //By checking writing password we see that password has some
+    // characters that are uppercase, lowercase, number or symbol.
+    public int PasswordStrength() {
+        String s = this.Value;
+        boolean UsedUpper = false;
+        boolean UsedLower = false;
+        boolean UsedNumber = false;
+        boolean UsedSymbol = false;
+        int type;
+        int Score = 0;
+
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            type = CharType(c);
+
+            if (type == 1) UsedUpper = true;
+            if (type == 2) UsedLower = true;
+            if (type == 3) UsedNumber = true;
+            if (type == 4) UsedSymbol = true;
+        }
+
+        if (UsedUpper) Score += 1;
+        if (UsedLower) Score += 1;
+        if (UsedNumber) Score += 1;
+        if (UsedSymbol) Score += 1;
+
+        if (s.length() >= 8) Score += 1;
+        if (s.length() >= 16) Score += 1;
+
+        //we return score that is increased by password's character
+        //types.
+        return Score;
+    }
 }
