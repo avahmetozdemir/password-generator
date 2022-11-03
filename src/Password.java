6a2c768af030448a97e7a94c0503275e1573ab9b
avahmetozdemir <avamertozdemir@gmail.com>
2022-11-03 17:32:05 +0300

    03- add chartype func.

--- src/Password.java
+++ src/Password.java
@@ -9,5 +9,31 @@ public class Password {
         Length = str.length();
     }
 
+    // in CharType func. we use ASCII Codes to get characters
+    // for password validation.
+    public int CharType(char C) {
+        int value;
+
+        // Char is Uppercase Letter
+        if ((int) C >= 65 && (int) C <= 90)
+            value = 1;
+
+            // Char is Lowercase Letter
+        else if ((int) C >= 97 && (int) C <= 122) {
+            value = 2;
+        }
+
+        // Char is Numbers
+        else if ((int) C >= 48 && (int) C <= 57) {
+            value = 3;
+        }
+
+        // Char is Symbol
+        else {
+            value = 4;
+        }
+
+        return value;
+    }
 
 }
